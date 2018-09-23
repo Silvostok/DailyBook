4b02abd84254388ac8bb33e6d4c42ec7eba78bd7
Miguel Angel Rubio Caballero <Miguel.Rubio.Caballero@hva.nl>
2018-09-23 20:22:55 +0200

    Initial Commit

---
+++ DailyBook/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DailyBook
+//
+//  Created by Miguel Angel Rubio Caballero on 11/09/2018.
+//  Copyright © 2018 Miguel Angel Rubio Caballero. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
